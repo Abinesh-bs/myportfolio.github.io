0871861a14555759f010e55f786654cb43d8aacd
Abinesh Bs <abineshnesh07@gmai.lcom>
2025-04-23 12:16:17 +0530

    web url change

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "560272406"
+    serviceWorkerVersion: "4217455467"
   }
 });
